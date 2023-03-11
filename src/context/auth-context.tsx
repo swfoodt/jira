dc9bace2611ea60e3c0bb5f2117d53436e5173d4
swfoodt <1801263727@qq.com>
2023-03-11 16:52:34 +0800

    Revert "feat：添加登录状态维持"
    This reverts commit 126d2a3028c1a5d47617a9c7d9c9b1cb1736f0a1.

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -2,22 +2,10 @@ import React, { ReactNode } from "react"
 import { useState } from "react"
 import * as auth from "auth-provider"
 import { User } from "screens/project-list/search-panel"
-import { http } from "utils/http"
-import { useMount } from "utils"
 
 interface AuthForm {
     username: string
     password: string
-}
-
-const bootstrapUser= async () => {
-    let user = null
-    const token = auth.getToken()
-    if(token){
-        const data = await http('me', {token})
-        user = data.user
-    }
-    return user
 }
 
 const AuthContext = React.createContext<{
@@ -35,10 +23,6 @@ export const AuthProvider = ({children} : { children: ReactNode }) => {
     const login = (form: AuthForm) => auth.login(form).then(setUser)
     const register = (form: AuthForm) => auth.register(form).then(setUser)
     const logout = () => auth.logout().then(user => setUser(null))
-
-    useMount(() => {
-        bootstrapUser().then(setUser)
-    })
 
     return <AuthContext.Provider children={children} value={{user, login, register, logout}}/>
 }
