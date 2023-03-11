eddec2d2df6d5f0e475eaa7e52019800fe804686
swfoodt <1801263727@qq.com>
2023-03-11 19:58:26 +0800

    style：安装emotion，优化登录注册界面

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,8 +1,9 @@
 import { useAuth } from "context/auth-context"
-import { Button, Form, Input } from 'antd'
+import { Form, Input } from 'antd'
+import { LongButton } from "unauthenticated-app"
 
 export const LoginScreen = () => {
-    const {login, user} = useAuth()
+    const {login} = useAuth()
 
     // const handleSubmit = (event: FormEvent<HTMLFormElement>) =>{
     //     event.preventDefault()
@@ -25,7 +26,7 @@ export const LoginScreen = () => {
                 <Input placeholder="密码" type="password" id={'password'}/>
             </Form.Item>
             <Form.Item>
-                <Button htmlType="submit" type={'primary'}>登录</Button>
+                <LongButton htmlType="submit" type={'primary'}>登录</LongButton>
             </Form.Item>
         </Form>
     )
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,9 +1,9 @@
 import { useAuth } from "context/auth-context"
-import { FormEvent } from "react"
-import { Button, Form, Input } from 'antd'
+import { Form, Input } from 'antd'
+import { LongButton } from "unauthenticated-app"
 
 export const RegisterScreen = () => {
-    const {register, user} = useAuth()
+    const {register} = useAuth()
 
     // const handleSubmit = (event: FormEvent<HTMLFormElement>) =>{
     //     event.preventDefault()
@@ -26,7 +26,7 @@ export const RegisterScreen = () => {
                 <Input placeholder="密码" type="password" id={'password'}/>
             </Form.Item>
             <Form.Item>
-                <Button htmlType="submit" type={'primary'}>注册</Button>
+                <LongButton htmlType="submit" type={'primary'}>注册</LongButton>
             </Form.Item>
         </Form>
     )
