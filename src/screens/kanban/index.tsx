05c7f38a4b4969a810e24408aaa3713e6b09579c
swfoodt <1801263727@qq.com>
2023-03-25 20:33:36 +0800

    feat:  看板任务的删除

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,34 +1,47 @@
 import styled from "@emotion/styled";
 import { useDocumentTitle } from "utils";
 import { KanbanColumn } from "./kanban-column";
-import { useKanbanSearchParams, useProjectInUrl } from "screens/kanban/util";
+import { useKanbanSearchParams, useProjectInUrl, useTasksSearchParams } from "screens/kanban/util";
 import { useKanbans } from "utils/kanban";
 import { SearchPanel } from "./search-panel";
 import { ScreenContainer } from "components/lib";
+import { TaskModal } from "./task-modal";
+import { CreateKanban } from "./create-kanban";
+import { Spin } from "antd";
+import { useTasks } from "utils/task";
 
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
 
   const { data: currentProject } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(
+    useKanbanSearchParams()
+  );
+  const { isLoading: taskIsLoading } = useTasks(useTasksSearchParams());
+  const isLoading = taskIsLoading || kanbanIsLoading;
   return (
     <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn kanban={kanban} key={kanban.id} />
-        ))}
-      </ColumnsContainer>
+      {isLoading ? (
+        <Spin size={"large"} />
+      ) : (
+        <ColumnsContainer>
+          {kanbans?.map((kanban) => (
+            <KanbanColumn kanban={kanban} key={kanban.id} />
+          ))}
+          <CreateKanban />
+        </ColumnsContainer>
+      )}
+      <TaskModal />
     </ScreenContainer>
   );
 };
 
-const ColumnsContainer = styled.div`
+export const ColumnsContainer = styled.div`
   display: flex;
   overflow-x: scroll;
   flex: 1;
-
   
   ::-webkit-scrollbar {
     display: none;
